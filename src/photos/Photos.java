f9630befec29c519b7abc6f3501d88eb3be1640c
Benjamin Ker <kerbenjamin@gmail.com>
2017-11-16 03:23:22 -0500

    login initial
    fix reference and change to FXMLLoader

--- src/photos/Photos.java
+++ src/photos/Photos.java
@@ -4,17 +4,23 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 
 public class Photos extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("LoginController.fxml").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene scene = new Scene(root);
+
+			primaryStage.setTitle("Login");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
